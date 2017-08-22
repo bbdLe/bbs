13db911de2205c9c523ab206de22545d1739c443
bbdLe <dalibaxiaoliba@gmail.com>
2017-08-22 09:52:57 -0400

    delete

--- start-bbs.sh
+++
@@ -1,7 +0,0 @@
-export MAIL_USERNAME="dalibaxiaoliba19@gmail.com"
-export MAIL_PASSWORD="lin8638289"
-export C_FORCE_ROOT=1
-source /6abbs/venv/bin/activate
-celery worker -A celery_runner --loglevel=info&
-python /6abbs/manage.py runserver -h 0.0.0.0 -p 80
-
